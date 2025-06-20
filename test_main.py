4b13569dd720044ce267de19e5fbd55b0a6138c2
LH <larshaas@ifi.uio.no>
2025-06-20 12:42:06 +0200

    Should not pass

--- test_main.py
+++ test_main.py
@@ -7,5 +7,5 @@ async def test_read_main():
     transport = ASGITransport(app=app)
     async with AsyncClient(transport=transport, base_url="http://test") as ac:
         response = await ac.get("/")
-        assert response.status_code == 200
+        assert response.status_code == 300
         assert "<h1>Home page</h1>" in response.text
